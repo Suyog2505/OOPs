50da0d86f6656e713b53ec22928833e82468df42
Suyog2505 <suyogpomdharane2505@gmail.com>
2025-01-15 11:16:06 +0530

    Assignment 1
    Assignment 1 Program 1

---
+++ oopexp1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+main() {
+   int a = 50;
+   int b = 60;
+   int c ;
+   c = a + b;
+   std::cout <<"Sum="<<c<<"\n";
+   c = a - b;
+    cout << "Sub="<<c<<"\n" ;
+   c = a * b;
+ 	cout << "prod="<<c<<"\n" ;   
+   c = a / b;
+ 	cout << "Div="<<c<<"\n" ;   
+   c = a % b;
+ 	cout << "Sub="<<c<<"\n" ;   return 0;
+}
