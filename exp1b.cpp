3f5c46dfdcb632bbae475e9e5d1bff13c5f286d0
Suyog2505 <suyogpomdharane2505@gmail.com>
2025-01-16 15:32:05 +0530

    Add files via upload

---
+++ exp1b.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+
+    cout << "Enter three numbers: ";
+    cin >> a >> b >> c;
+
+    if (a >= b && a >= c) {
+    std::cout << "largest is" << a <<"\n" ;
+    }
+    else if (b >= a && b >= c) {
+    std::cout << "largest is " << b << "\n";
+    }
+    else {
+    std::cout << "largest is " << c <<"\n" ;
+    }
+
+    return 0;
+}
+
